1866f34fdc36550aa1bd7d0550c77490bfcf8887
usuario <usuario@usuario-PC>
2014-10-06 21:37:52 +0200

    Minkowsky corregida, horas añadidas.

--- Kmeans_weka_lib/src/org/packmodelo/MinkowskyDistance.java
+++ Kmeans_weka_lib/src/org/packmodelo/MinkowskyDistance.java
@@ -1,21 +1,26 @@
 package org.packmodelo;
 
+import java.util.ArrayList;
+
 public class MinkowskyDistance {
 	
 	public MinkowskyDistance(){}
 	
 	/*
-	 * Nota: podemos hacerlo de dos formas, que se cree la clase solo para calcular, o crear una clase con setP y set instancias y que después tenga
+	 * TODO Nota: podemos hacerlo de dos formas, que se cree la clase solo para calcular, o crear una clase con setP y set instancias y que después tenga
 	 * una función sin parámetros, con solo la ejecución y lo que devuelva.
 	 */
 	
 	public double calculateDistance(Instance x, Instance y, double p)
 	{
 		double res = 0.0;
+		ArrayList<Double> attX = x.getDoubleAtt();
+		ArrayList<Double> attY = y.getDoubleAtt();
 		
-		for (int i = 0; i<10; i++) //para el nº de atributos
+		
+		for (int i = 0; i<attX.size(); i++) //para el nº de atributos
 		{
-			res = res + Math.pow(Math.abs(1-1),p); //aqui x e y
+			res = res + Math.pow(Math.abs((attX.get(i).doubleValue()) - attY.get(i).doubleValue()), p); //aqui x e y
 		}
 		
 		res = Math.pow(res, 1/p);
