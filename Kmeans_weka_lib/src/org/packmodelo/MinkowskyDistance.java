62b2ac01bc8597a14f75305827ef5cbf835234d5
usuario <usuario@usuario-PC>
2014-10-06 20:38:56 +0200

    Añadida una nota y constructora

--- Kmeans_weka_lib/src/org/packmodelo/MinkowskyDistance.java
+++ Kmeans_weka_lib/src/org/packmodelo/MinkowskyDistance.java
@@ -2,6 +2,13 @@ package org.packmodelo;
 
 public class MinkowskyDistance {
 	
+	public MinkowskyDistance(){}
+	
+	/*
+	 * Nota: podemos hacerlo de dos formas, que se cree la clase solo para calcular, o crear una clase con setP y set instancias y que después tenga
+	 * una función sin parámetros, con solo la ejecución y lo que devuelva.
+	 */
+	
 	public double calculateDistance(Instance x, Instance y, double p)
 	{
 		double res = 0.0;
