804d92a005e4a603075440e1b3f9b6d64c15282e
Iñigo Sanchez <jisanchez003@ehu.es>
2014-10-19 22:38:26 +0200

    report

--- Kmeans/src/dm/clustering/kmeans/Kmeans.java
+++ Kmeans/src/dm/clustering/kmeans/Kmeans.java
@@ -1,12 +1,29 @@
 package dm.clustering.kmeans;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import dm.clustering.utils.DataLoader;
 import dm.clustering.utils.InputHandler;
@@ -174,6 +191,8 @@ public class Kmeans {
 			System.out.println("Con el codeword: \n"+s);
 			System.out.println("===========================================================================");
 		}
+		
+		//report(clusters, codebook, InputHandler.getMiHandler().getDataPath());
 	}
 
 	/**
@@ -297,4 +316,95 @@ public class Kmeans {
 		}
 		return true;	
 	}
+
+	public static void report(Cluster[] clusters, ArrayList<Instance> codebook, String path){
+		
+		
+		final String LOG_TAG = Kmeans.class.getSimpleName();
+		Calendar calendar = new GregorianCalendar(); // Fecha y hora actuales.
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd-HHmm"); // Formato de la fecha.
+		String dateS = dateFormat.format(calendar.getTime()); // Fecha y hora actuales formateadas.		
+		String datePDF = new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime());
+		//String filePath = "Resultados/"+dateS+"resultados.txt";
+		String parent = "Resultados";
+		File directory =new File(parent);
+		directory.mkdir();
+		File myPath =new File(parent+"/"+dateS+"_"+path.substring(5, path.length()-5)+"_resultados.pdf");
+			try {
+				FileOutputStream file = new FileOutputStream(myPath);
+				Document docu = new Document();
+				PdfWriter.getInstance(docu, file);
+				docu.open();
+				docu.addTitle("ANALISIS DE RESULTADOS");
+				docu.addCreationDate();
+				docu.addAuthor("Iñigo Sánchez Méndez\n&\nJosu Rodríguez Azpeleta");
+				docu.addCreator("Iñigo Sánchez & Josu Rodríguez");
+				Font font =FontFactory.getFont("arial",   // fuente
+						22,                            // tamaño
+						Font.ITALIC,                   // estilo
+						BaseColor.BLACK);
+				font.setStyle(Font.UNDERLINE);
+				Paragraph titulo = new Paragraph("Datos de agrupamiento de la instancias en "+clusters.length+" Clusters",font);
+				titulo.setAlignment(Paragraph.ALIGN_CENTER);
+				docu.add(titulo); 
+
+				font.setStyle(Font.ITALIC);
+				font.setSize(font.getSize()-6);
+				Paragraph autor = new Paragraph("Autor: Iñigo Sanchez Mendez",font);
+				autor.setAlignment(Paragraph.ALIGN_CENTER);
+				docu.add(autor);
+
+				Paragraph fecha =  new Paragraph(datePDF);
+				fecha.setAlignment(Paragraph.ALIGN_CENTER);
+				fecha.getFont().setSize(10);
+				docu.add(fecha);
+
+
+
+				docu.newPage();
+
+				Paragraph archivo = new Paragraph("Conjunto de instancias en el archivo "+InputHandler.getMiHandler().getFileExtension()+"\n\n");
+				archivo.setAlignment(Paragraph.ALIGN_MIDDLE);
+				docu.add(archivo);
+
+				PdfPTable table = new PdfPTable(6);
+
+				//Array que devuelve, para cada instancia el cluster al que pertenece, empezando por 0
+				//Tiene tantos elementos, como número de instancias.
+				for(int i=0;i<clusters.length;i++) 
+				{
+					if(!clusters[i].getListaInstances().isEmpty())
+					{
+						for(int j=0;j<clusters[i].getListaInstances().size();j++)
+						{
+							System.out.printf("Instance %d -> Cluster %d \n", j, i);
+							//TODO args para guardar en archivo si el usuario lo pide.
+							//String output = "Instance "+i+" -> Cluster "+ clusterNum+"\n";
+							String instance ="Instance "+j;
+							String cluster ="Cluster "+ i;
+							PdfPCell cell = new PdfPCell();
+							cell.setBackgroundColor(BaseColor.ORANGE);
+							cell.addElement(new Phrase(instance));
+							table.addCell(cell);
+							table.addCell(cluster);
+							//docu.add(new Paragraph(output));
+							//dl.SaveFile(filePath, output, false);
+						i++;
+
+						}
+					}
+				}
+				docu.add(table);
+				docu.close();
+			} 
+			catch (FileNotFoundException e) 
+			{
+				Logger.getLogger(LOG_TAG).log(Level.SEVERE, "No se encuentra el archivo en el sistema");
+			} 
+			catch (DocumentException e) 
+			{
+				Logger.getLogger(LOG_TAG).log(Level.SEVERE, "No se puede crear el reporte");
+			}
+	}
 }
+
